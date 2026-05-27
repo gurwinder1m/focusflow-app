1e8ef40a6ae7e4342d07ad2832c06a6f18d52212
Gurwinder <unsatisfyg@gmail.com>
2026-05-27 17:51:21 +0530

    fix cors url

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -14,7 +14,7 @@ export const env = {
   nodeEnv: process.env.NODE_ENV || 'development',
   port: Number(process.env.PORT || 5050),
   clientUrl:
-  process.env.CLIENT_URL || 'https://focusflow-app-client.vercel.app',
+  process.env.CLIENT_URL || 'https://focusflow-app-client-avk3a5ko8-gurwinder1ms-projects.vercel.app',
   mongoUri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/focusflow',
   jwtSecret: process.env.JWT_SECRET || 'development-only-change-me',
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || '7d'
